4bdaf4175899c92849a5281bbf209151e2e187bc
Timo <43245438+TheRisenPhoenix@users.noreply.github.com>
2022-08-04 09:17:00 +0200

    Minor logging improvements

--- Python-SourceCode/app.py
+++ Python-SourceCode/app.py
@@ -1,4 +1,5 @@
 import json
+import os.path
 
 import flask
 from flask import Flask, request
@@ -11,15 +12,26 @@ classificator = Classificator.Classificator()
 
 @app.route("/AiService/PreloadModels")
 def prepare_models_requested():
+    print("Preloading of models requested...")
+    if classificator.models_loaded:
+        print("Models have already been loaded before.")
+        return flask.Response(status=200)
+
     classificator.load_models()
+    print("Loading of models done.")
     return flask.Response(status=200)
 
 
 @app.route("/AiService/Classification", methods=["POST"])
 def classification_requested():
+    print("Classification requested...")
     content = request.get_json()
     path_frontal = content["PathFrontal"]
     path_lateral = content["PathLateral"]
+    if not path_frontal or not os.path.exists(path_frontal) or not path_lateral or not os.path.exists(path_lateral):
+        print("At least one of the requested paths does not exist.")
+        return flask.Response(status=400)
     activation_l, activation_f = classificator.do_classification(path_frontal, path_lateral)
     result = {'OutputFrontal': activation_f, 'OutputLateral': activation_l}
+    print("Classification done.")
     return json.dumps(result)
